6706d6ad2f7d076e046fc7dc24a81829ecd2501a
Abrahamcepe <abrahamgolf@gmail.com>
2021-01-25 20:11:22 -0600

    Configured functionality for adding new recipes to the database

--- src/components/admin/Recetas.js
+++ src/components/admin/Recetas.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import firebase from '../../database/firebase';
-import { FormControl, TextField, Button } from '@material-ui/core';
+import { FormControl, TextField, Button, FormGroup, FormLabel, FormControlLabel, Checkbox } from '@material-ui/core';
 
 function Recetas() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
     const [ingredients, setIngredients] = useState([{ingredient: ""}]);
     const [steps, setSteps] = useState([{step: ""}]);
     const [notes, setNotes] = useState([{note: ""}]);
     const [newCategory, setNewCategory] = useState('');
     const [categories, setCategories] = useState([]);
+    const [selectedCategories, setSelectedCategories] = useState([]);
+    const [image, setImage] = useState(null);
+    const [progress, setProgress] = useState(0);
 
     useEffect(() => {
         firebase.db.collection("recetas").onSnapshot(querySnapshot => {
@@ -20,15 +25,15 @@ function Recetas() {
                 }
                 cat.push(data);
             })
+            console.log(cat);
             setCategories(cat);
         })
-    }, [categories])
+    }, [])
 
     // handle input change
     const handleInputChange = (e, index) => {
-        const { name, value } = e.target;
         const list = [...ingredients];
-        list[index][name] = value;
+        list[index].ingredient = e.target.value;
         setIngredients(list);
     };
 
@@ -46,9 +51,8 @@ function Recetas() {
 
     // handle input change - Steps
     const handleInputChangeSteps = (e, index) => {
-        const { name, value } = e.target;
         const list = [...steps];
-        list[index][name] = value;
+        list[index].step = e.target.value;
         setSteps(list);
     };
 
@@ -66,9 +70,8 @@ function Recetas() {
 
     // handle input change - Notes
     const handleInputChangeNotes = (e, index) => {
-        const { name, value } = e.target;
         const list = [...notes];
-        list[index][name] = value;
+        list[index].note = e.target.value;
         setNotes(list);
     };
 
@@ -84,7 +87,7 @@ function Recetas() {
         setNotes([...notes, { note: "" }]);
     };
 
-    const addCategory =  (event) => {
+    const addCategory = (event) => {
         event.preventDefault();
         firebase.db.collection("recetas").add({
             titulo: newCategory,
@@ -93,31 +96,100 @@ function Recetas() {
         setNewCategory("");
     }
 
-    /* const uplodadImage = async () => {
-        const childPath = `recetas/${firebase.firebase.auth().currentUser.uid}/${Math.random().toString(36)}`
-        const response = await fetch(uri);
-        const blob = await response.blob();
-
-        const task = firebase.storage
-            .ref()
-            .child(childPath)
-            .put(blob);
-
-        const taskProgress = snapshot => {
-            console.log(`transferred: ${snapshot.bytesTransferred}`)
+    const checkBoxChange = (event) => {
+        
+        if(event.target.checked  && selectedCategories.indexOf(event.target.name) == -1){
+            const data = selectedCategories;
+            data.push(event.target.name);
+            setSelectedCategories(data);
+        } else{
+            const data = selectedCategories;
+            data.splice(data.indexOf(event.target.name),1)
+            setSelectedCategories(data);
         }
-        const taskCompleted = () => {
-            task.snapshot.ref.getDownloadURL().then((snapshot) => {
-                savePostData(snapshot)
-                console.log(snapshot)
-            })
-        }
-        const taskError = snapshot => {
-            console.log(snapshot)
-        }
+        
 
-        task.on("state_changed", taskProgress, taskError, taskCompleted);
-    } */
+        console.log(selectedCategories);
+        
+        
+    }
+
+    const getCategoryID = (category) => {
+        let categoryID = "";
+        categories.map(cat => {
+            console.log(cat.titulo);
+            console.log(cat.titulo ===  category);
+            if(cat.titulo === category){
+                categoryID = cat.id;
+            }
+        })
+        return categoryID;
+    }
+
+    const handleImageChange = (e) => {
+        if(e.target.files[0]){
+            setImage(e.target.files[0]);
+            console.log(e.target.files[0]);
+        }
+    };
+
+    const handleUpload = () => {
+        selectedCategories.forEach(category => {
+            const categoryID = getCategoryID(category);
+            console.log("category: ",category);
+            const uploadTask = firebase.storage.ref(`recetas/${category}/${image.name}`).put(image);
+            uploadTask.on(
+                "state_changed",
+                (snapshot) =>  {
+                    // progress  function
+                    const progress = Math.round(
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    );
+                    setProgress(progress)
+                },
+                (error) => {
+                    // error function
+                    console.log(error);
+                    alert(error.message);
+                },
+                () => {
+                    // complete function
+                    firebase.storage
+                        .ref(`recetas/${category}`)
+                        .child(image.name)
+                        .getDownloadURL()
+                        .then(url => {
+                            console.log("category ID", categoryID);
+                            firebase.db.collection("recetas").doc(categoryID).collection(category).add({
+                                descripcion: description,
+                                titulo: title,
+                                image: url
+                            })
+                            .then(docRef => {
+                                ingredients.forEach(ing => {
+                                    firebase.db.collection("recetas").doc(categoryID).collection(category).doc(docRef.id).collection("ingredientes").add({
+                                        texto: ing.ingredient
+                                    })
+                                });
+                                steps.forEach(step => {
+                                    firebase.db.collection("recetas").doc(categoryID).collection(category).doc(docRef.id).collection("proceso").add({
+                                        texto: step.step
+                                    })
+                                });
+                                notes.forEach(note => {
+                                    firebase.db.collection("recetas").doc(categoryID).collection(category).doc(docRef.id).collection("notas").add({
+                                        texto: note.note
+                                    })
+                                });
+                            })
+                            setProgress(0);
+                            setImage(null);
+                        });
+                }
+            )
+        })
+        
+    }
 
     return (
         <div>
@@ -141,8 +213,8 @@ function Recetas() {
             </form>
             <form>
                 <FormControl>
-                    <TextField id="filled-basic" variant="filled" placeholder="Título"/>
-                    <TextField id="filled-basic" variant="filled" placeholder="Descripción"/>
+                    <TextField id="filled-basic" variant="filled" placeholder="Título" value={title} onChange={e => setTitle(e.target.value)}/>
+                    <TextField id="filled-basic" variant="filled" placeholder="Descripción" value={description} onChange={e => setDescription(e.target.value)}/>
                     <p>Ingredientes</p>
                     {ingredients.map((item, i) => {
                         return (
@@ -153,7 +225,6 @@ function Recetas() {
                                 />
 
                             </div>
-                            
                         )
                     })}
                     <Button onClick={handleAddClick}>
@@ -192,6 +263,21 @@ function Recetas() {
                     })}
                     <Button onClick={handleAddClickNotes}>
                         Add
+                    </Button>
+                    <FormLabel component="legend">Select categories</FormLabel>
+                    <FormGroup>
+                        {categories && categories.map((category) => (
+                            <FormControlLabel
+                                control={<Checkbox onChange={checkBoxChange} name={category.titulo} />}
+                                label={category.titulo}
+                            />
+                        ))}
+                    </FormGroup>
+
+                    <progress className="imageUpload__progress" value={progress} max="100"/>
+                    <input type="file" onChange={handleImageChange}/>
+                    <Button onClick={handleUpload}>
+                        Upload Image
                     </Button>
                 </FormControl>
             </form>
