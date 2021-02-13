0c63c75b08df54c9ba1d80a66c02f98dfc59fb76
Abrahamcepe <abrahamgolf@gmail.com>
2021-02-13 11:56:03 -0600

    Styled footer

--- src/components/home/others/Footer.js
+++ src/components/home/others/Footer.js
@@ -1,32 +1,103 @@
 import React from 'react';
-import { makeStyles } from "@material-ui/core/styles";
-//import { Col, Row } from 'react-bootstrap';
+
+//Link
 import { Link } from "react-router-dom";
 import { HashLink } from 'react-router-hash-link';
+
+//MaterialUI
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
+import MailIcon from '@material-ui/icons/Mail';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import { Button } from '@material-ui/core'
+
+//Colors
+import { PRIMARY,  
+        BLACK_BUTTON_PRIMARY, 
+        BLACK_BUTTON_SECONDARY,  
+} from "../../../resources/Colors";
+
+//Bootstrap
+import { Col, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const useStyles = makeStyles((theme) =>({
     backgroundContainer: {
         width: '100vw',
-        height: '100vh',
-        minHeight: '800px',
-        backgroundImage: "url(./img/home/landing/background.jpg)",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        [theme.breakpoints.up('992')]: {
-            backgroundAttachment: 'fixed'
-        }
+        background: PRIMARY,
+        padding: '50px'
     },
+    logoImg: {
+        width: '200px'
+    },
+    infoContainer: {
+        textAlign: 'center'
+    },
+    socialIcons: {
+        fontSize: '45px',
+        cursor: 'pointer',
+        transition: 'transform 500ms',
+        '&:hover': {
+            transform: 'scale(1.2)'
+        }
+    }
 }));
 
+const BlackButton = withStyles((theme) => ({
+  root: {
+    color: PRIMARY,
+    backgroundColor: BLACK_BUTTON_PRIMARY,
+    borderRadius: "100px",
+    transition: "transform 450ms",
+    fontWeight: "bolder",
+    padding: "10px 20px",
+    margin: 'auto',
+    marginBottom: '20px',
+    width: '80%',
+    '&:hover': {
+      backgroundColor: BLACK_BUTTON_SECONDARY,
+      transform: "scale(1.08)",
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: BLACK_BUTTON_SECONDARY,
+      border: 'none',
+    },
+    '&:focus': {
+      backgroundColor: BLACK_BUTTON_SECONDARY,
+    },
+    '&:disabled': {
+        opacity: '0.7',
+        color: 'black'
+    }
+  },
+}))(Button);
 
 function Footer({classes}) {
     classes = useStyles();
 
     return (
         <div className={classes.backgroundContainer}>
-            <h1>footer</h1>
+            <Row>
+                <Col sm={6} md={3} className={classes.infoContainer}>
+                    <img className={classes.logoImg} src="./img/home/landing/logo.png"/>
+                </Col>
+                <Col sm={6} md={3} className={classes.infoContainer}>
+                    <p><PhoneIphoneIcon/> (999) 908 8092</p>
+                    <p><MailIcon/> admin@nutzen.mx</p>
+                </Col>
+                <Col sm={6} md={3} className={classes.infoContainer}>
+                    <BlackButton>Ver recetas</BlackButton>
+                </Col>
+                <Col sm={6} md={3} className={classes.infoContainer}>
+                    <div className={classes.menuItemContainerSocial}>
+                        <InstagramIcon className={classes.socialIcons} style={{marginRight: '10px'}}/>
+                        <FacebookIcon className={classes.socialIcons}/>
+                    </div>
+                </Col>
+            </Row>
+            
         </div>
     )
 }
